207889b20669d72677a863a6d96a57e24eca3dbc
hanasuru <faqih.insani@ymail.com>
2024-08-13 05:47:07 +0700

    Add color-linting and exception handling

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # vol3_profile_builder
 
-vol3_profile_builder is a script to build a volatility3 profile based on given kernel version. It's worked by converting vmlinux with [dwarf2json](https://github.com/volatilityfoundation/dwarf2json). Mainly tested on Ubuntu-based virtual machine
+vol3_profile_builder is a script to build a volatility3 profile based on given kernel version. It's worked by converting vmlinux from [ddebs repository](http://ddebs.ubuntu.com/pool/main/l/) with [dwarf2json](https://github.com/volatilityfoundation/dwarf2json). Mainly tested on Ubuntu-based virtual machine
 
 ## Usage
 
 ```bash
 $ ./build.sh
-Usage: ./build.sh kernel_version
-Example: ./build.sh 5.15.0-118
+Usage: ./build.sh kernel_version architecture
+Example: ./build.sh 5.15.0-118 amd64
 ```
 
 ## Authors
--- build.sh
+++ build.sh
@@ -1,82 +1,104 @@
 #!/bin/bash
-BASE_URL='http://ddebs.ubuntu.com/pool/main/l/linux/'
+GENERIC_BASE_URL='http://ddebs.ubuntu.com/pool/main/l/linux/'
+HWE_BASE_URL='http://ddebs.ubuntu.com/pool/main/l/linux-hwe/'
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
 
 install_requirement(){
     if ! which go > /dev/null 2>&1; then
-        echo "[x] go is not in PATH. Installing golang-go..."
+        echo -e "${YELLOW}⚠️ go is not in PATH.${NC}"
+        echo -e "${GREEN}✅ Installing golang-go dependencies${NC}"
         sudo apt install golang-go build-essential -y
         echo
     fi
 
     if ! which dwarf2json > /dev/null 2>&1; then
-        echo "[x] dwarf2json is not in PATH. Installing dwarf2json..."
+        echo -e "${YELLOW}⚠️ dwarf2json is not in PATH${NC}"
+        echo -e "${GREEN}✅ Compiling and Installing dwarf2json${NC}"
         git clone https://github.com/volatilityfoundation/dwarf2json
         cd dwarf2json && go build && chmod +x dwarf2json && sudo mv dwarf2json /usr/bin/
         echo && cd ..
     fi
 }
 
-get_ddeb_DDEB_PATH(){
-    LIST_FILE=$(curl -s $BASE_URL)
-    DDEB_PATH=$(echo $LIST_FILE | grep -oP "(?<=href=\")linux-image-(unsigned-)?$1-generic-dbgsym_$1[\d\.\-]+_amd64.ddeb")
+get_ddeb_path(){
+    LIST_FILE=$(curl -s $GENERIC_BASE_URL)
+    DDEB_PATH=$(echo $LIST_FILE | grep -oP "(?<=href=\")linux-image-(unsigned-)?$1-generic-dbgsym_$1[\d\.\-]+_$2.ddeb")
+    
     echo $DDEB_PATH
+    if [ -z "${DDEB_PATH}" ]; then
+        exit 1
+    fi
 }
 
 download_ddeb(){
-    echo -e "[x] $1 does not exist. Start downloading DDEB files..."
-    DDEB_URL="${BASE_URL}$1"
+    echo -e "${YELLOW}⚠️ $1 does not exist yet${NC}"
+    echo -e "${GREEN}✅ Downloading $1 from repository${NC}"
+    DDEB_URL="${GENERIC_BASE_URL}$1"
     wget -P /tmp $DDEB_URL
     echo
 }
 
 unpack_vm_kernel(){
-    echo -e "[x] Unpacking vmlinux file..."
+    echo -e "${GREEN}✅ Unpacking vmlinux file${NC}"
     ar x /tmp/$1 data.tar.xz
     tar -xI/usr/bin/xz -f data.tar.xz ./usr/lib/debug/boot/vmlinux-$2-generic
 }
 
 generate_dwarf_file(){
-    echo -e "[x] Generating DWARF JSON file..."
-    dwarf2json linux --elf ./usr/lib/debug/boot/vmlinux-$1-generic | xz > "linux-image-${1}-generic.json.xz"
+    echo -e "${GREEN}✅ Generating DWARF JSON file${NC}"
+    dwarf2json linux --elf ./usr/lib/debug/boot/vmlinux-$1-generic | xz > "linux-image-${1}-generic_$2.json.xz"
 }
 
-if [ "$#" -eq 1 ]; then
+cleanup_file(){
+    rm -f $1 data.tar.xz 
+}
+
+if [ "$#" -eq 2 ]; then
     KERNEL_VERSION=$1
+    ARCH=$2
+    
     install_requirement
     if [ $? -ne 0 ]; then
-        echo "Error occurred in install_requirement."
+        echo -e "${RED}❌ Error occurred while installing dependencies${NC}"
         exit 1
     fi
 
-    DDEB_PATH=$(get_ddeb_DDEB_PATH $KERNEL_VERSION)
+    DDEB_PATH=$(get_ddeb_path $KERNEL_VERSION $ARCH)
     if [ $? -ne 0 ]; then
-        echo "Error occurred in get_ddeb_DDEB_PATH."
+        echo -e "${RED}❌ Requested linux $KERNEL_VERSION kernel for $ARCH is nowhere to be found${NC}"
         exit 1
     fi
 
+    rm /tmp/*.ddeb
     if [ ! -e "/tmp/$DDEB_PATH" ]; then
         download_ddeb $DDEB_PATH
         if [ $? -ne 0 ]; then
-            echo "Error occurred in download_ddeb."
+            echo -e "${RED}❌ Error occurred while downloading $DDEB_PATH${NC}"
             exit 1
         fi
     fi
     
     unpack_vm_kernel $DDEB_PATH $KERNEL_VERSION
     if [ $? -ne 0 ]; then
-        echo "Error occurred in unpack_vm_kernel."
+        echo -e "${RED}❌ Error occurred while unpacking ddeb kernel file${NC}"
         exit 1
     fi
 
-    generate_dwarf_file $KERNEL_VERSION
+    generate_dwarf_file $KERNEL_VERSION $ARCH
     if [ $? -ne 0 ]; then
-        echo "Error occurred in generate_dwarf_file."
+        echo -e "${RED}❌ Error occurred while generating DWARF-json file${NC}"
         exit 1
     fi
 
-    echo "[v] Saved profile to linux-image-${1}-generic.json.xz"
+    cleanup_file $DDEB_PATH
+
+    echo -e "${GREEN}✅ Saved profile to linux-image-${1}-generic.json.xz${NC}"
 else
     echo
-    echo "Usage: $0 kernel_version"
-    echo "Example: $0 5.15.0-118"
+    echo "Usage: $0 kernel_version architecture"
+    echo "Example: $0 5.15.0-118 amd64"
 fi
